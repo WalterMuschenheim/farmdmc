c8fc7ad734d7e3c7a499f7f5d73dbf87ba2abd01
Walter Muschenheim <walter.muschenheim@gmail.com>
2020-09-03 15:06:45 -0400

    deleted text added to farmdmc/index.php for git test

--- wp-content/themes/farmdmc/index.php
+++ wp-content/themes/farmdmc/index.php
@@ -36,9 +36,6 @@ get_header();
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 *
-				 *
-				 * I am testing git
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
